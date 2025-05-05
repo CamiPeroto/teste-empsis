e2ccc3bc06d417373f2fb1fa093af2c59feafac6
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-05 19:59:04 -0300

    feat: implementando o  layout padrão

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
-</div>
+@extends('layouts.admin')
+
+@section('content')
+
+    {{-- Conteúdo do List --}}
+
+@endsection
