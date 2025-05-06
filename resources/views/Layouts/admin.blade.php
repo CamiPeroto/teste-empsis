cdbe14eadb23edea2de3e31e50ede10f0c12fb9c
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-06 12:45:08 -0300

    fix: estilizando a navbar

--- resources/views/Layouts/admin.blade.php
+++ resources/views/Layouts/admin.blade.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css', ,'resources/js/app.js')
+    @vite('resources/css/app.css')
     <title>Teste - Empsis</title>
 </head>
-<body>
-    {{-- <nav> Estilizar </nav> --}}
+
+<body class="bg-body">
+    <nav class="navbar">
+        <div class="navbar-container">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="navbar-logo">
+            <ul>
+                <div class="search-input">
+                    <li class="search-items"><x-heroicon-o-magnifying-glass class="w-5 h-5" />Pesquisar</li>
+                </div>
+            </ul>
+        </div>
+    </nav>
 
     <main>
         @yield('content')
     </main>
 
-    {{-- <footer> Estilizar </footer> --}}
-    
 </body>
+
 </html>
