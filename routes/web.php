feb04a54c603371723d9963545950197ac525095
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-07 17:32:53 -0300

    fix: definindo a rota index como padrão

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/index-user');
+});
+
 Route::get('/index-user', [UserController::class, 'index'])->name('user.index');
 Route::get('/show-user/{user}', [UserController::class, 'show'])->name('user.show');
 Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
